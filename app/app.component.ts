137d5ceaa3247f9eb14b8808dfde0aed9514c946
Vallabh Kansagara <vrkansagara@gmail.com>
2018-07-18 13:24:40 +0530

    adding extra code.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,76 @@
 import { Component } from '@angular/core';
 
+
+interface Child{
+  name: string;
+  age :number;
+}
+interface Passenger{
+  id: number,
+  name:string,
+  checkedIn:boolean,
+  checkedInDate: number | null,
+  children: Child[] | null
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   templateUrl: 'app.component.html'
 })
+
 export class AppComponent {
-  title : string;
+  name : string;
+  logo : string;
+  appName: string;
+  search : string ='';
+  passengers : Passenger[];
+
   constructor(){
-    this.title = 'Vallabh Kansagara';
+    this.name = 'Vallabh Kansagara';
+    this.logo = '/images/favicon.png'
+    this.appName = 'Skeleton Application'
+
+    this.passengers = [
+      {
+        id:1,
+        name:'VK',
+        checkedIn:true,
+        checkedInDate: null,
+        children : null
+      },
+      {
+        id:2,
+        name:'KK',
+        checkedIn:false,
+        checkedInDate:11312313125341245,
+        children : null
+      },
+      {
+        id:3,
+        name:'MK',
+        checkedIn:true,
+        checkedInDate:354614423442364423,
+        children : [
+          {
+            name:'Shyama',
+            age:3
+          },
+          {
+            name:'Vrunda',
+            age:1
+          }
+        ]
+      }
+    ];
+  }
+
+  handleClick(value  : string){
+    console.log(value)
+  }
+
+  handleChange(value :string){
+    console.log(value);
+    this.search = value;
   }
 }
